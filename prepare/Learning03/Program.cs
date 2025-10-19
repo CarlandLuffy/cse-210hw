4674b3b34272e6b5b10b1de0b305b5b18c5142c0
CarlandLuffy <jirenallday@gmail.com>
2025-10-18 20:28:30 -0600

    Hey Carl Lol me and the teacher named Carl!

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        f2.SetTop(6);
+        f2.SetBottom(1);
+
+        DisplayFraction(f1);
+        DisplayFraction(f2);
+        DisplayFraction(f3);
+        DisplayFraction(f4);
+    }
+
+    static void DisplayFraction(Fraction f)
+    {
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+        Console.WriteLine();
     }
 }

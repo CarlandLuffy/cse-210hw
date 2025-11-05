8d1cf9221587b71d45af11be3c20dc0aa1bba396
CarlandLuffy <jirenallday@gmail.com>
2025-11-05 09:04:41 -0700

    hi

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,34 @@
+using System;
+
+public abstract class Activity 
+{
+
+    private int _timer; 
+    private string _startMessage;
+
+   private string _endMessage;
+
+   public Activity(string name 
+   
+   public void DisplayInstructions()
+   {
+   public void PauseWithAnimation()
+   {
+
+    public void DisplayInstructions()
+   
+    Console.Writeline($"Welcome to the activity!");
+    Console.Writeline(_startmessage);
+    Console.Writeline();
+
+   
+   public void Run()
+   {
+
+
+
+
+
+
+
+   
